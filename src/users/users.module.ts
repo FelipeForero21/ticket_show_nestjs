6dfaf1211d7ea44d60191ce2ff38b9a60c0dd4ff
anacaszapata <anacaszapata@gmail.com>
2024-05-01 13:45:08 -0500

    feat (users.module): add entityrol to module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { Rol } from './entities/rol.entity';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User,Rol])],
   controllers: [UsersController],
   providers: [UsersService],
 })
